5b6aeae60fd9b1310efe97b8ebb3860447bbc772
Kevin Ball <kball8000@gmail.com>
2017-07-01 16:58:33 -0500

    initial commit for Favs using Angular 4 and Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Favorite Things - Getting Started';
+  favoriteColor = 'blue';
+  favoriteNumber = 0;
+
+  setColor = function(selectedColor: string): void {
+    console.log('heyo, colo2r: ' + selectedColor);
+    this.favoriteColor = selectedColor;
+  };
+  updateColor = function(): void {
+    console.log('you selected update colorR');
+  };
+
+  // setNum = (test) => this.favoriteNumber = (test) ? this.favoriteNumber++ : this.favoriteNumber--;
+  setNum = (test): void => {
+    console.log('running setNum, test: ' + test);
+    if (test) {
+      this.favoriteNumber++;
+    } else {
+      this.favoriteNumber--;
+    }
+    // this.favoriteNumber = (test) ? ++this.favoriteNumber : --this.favoriteNumber;
+  }
+
+  increaseNum = () => this.favoriteNumber++;
+  decreaseNum = () => this.favoriteNumber--;
+  resetNum = () => this.favoriteNumber = 0;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBhle4AY9_CETvQU5v4DTDA8Ets3KjrzNs",
+    authDomain: "kball17-favprod.firebaseapp.com",
+    databaseURL: "https://kball17-favprod.firebaseio.com",
+    projectId: "kball17-favprod",
+    storageBucket: "kball17-favprod.appspot.com",
+    messagingSenderId: "735512369737"
+  }
 };
