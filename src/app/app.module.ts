f7dbd5cb51966f295dd1847f1f34ce5f4499ccef
Kevin Ball <kball8000@gmail.com>
2017-07-03 06:57:07 -0500

    tried and failed to add ionic to this app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 // import { FormsModule } from '@angular/forms';
 // import { HttpModule } from '@angular/http';
+
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+
 import { AngularFireModule } from 'angularfire2'
 import { AngularFireDatabaseModule } from 'angularfire2/database'
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+// import { HomePage } from '../pages/home/home';   // Will probably make more sense if I use blank project.
 
 @NgModule({
   declarations: [
@@ -14,14 +21,26 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    IonicModule.forRoot(AppComponent),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule
     // FormsModule,
     // HttpModule
   ],
-  providers: [],
+  // entryComponents: [
+  //   AppComponent
+  //   // HomePage
+  // ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    // Facebook    
+  ],
+  // bootstrap: [IonicApp]
   bootstrap: [AppComponent]
 })
 export class AppModule {
   
 }
+``
