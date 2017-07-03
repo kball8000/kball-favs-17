d465b3b192821047c0f4e534d8645fe7db2483f5
Kevin Ball <kball8000@gmail.com>
2017-07-03 13:04:39 -0500

    got firebase working again, still do not know how to rollback in git...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -9,25 +11,43 @@ export class AppComponent {
   title = 'Favorite Things - Getting Started';
   favoriteColor = 'blue';
   favoriteNumber = 0;
+  items: FirebaseListObservable<any[]>;
+  myItems: Array<any> = [];
+
+  constructor(db: AngularFireDatabase) {
+    this.items = db.list('/');
+    this.items.forEach(x => console.dir(x))
+
+    this.items.forEach(x => {
+      this.myItems = x;
+    })
+    let database = firebase.database();
+    let itemsRef = database.ref('/');
+    itemsRef.on('value', function(el){
+      let obj = el.val();
+      for (let prop in obj){
+        console.log('prop: ', obj[prop]);
+      }
+    })
+  }
+
+  dbRef = firebase.database().ref('/');
+  itemsRef = firebase.database().ref('/items');
 
   setColor = function(selectedColor: string): void {
-    console.log('heyo, colo2r: ' + selectedColor);
     this.favoriteColor = selectedColor;
   };
   updateColor = function(): void {
-    console.log('you selected update colorR');
+    this.itemsRef.update({favoriteColor: this.favoriteColor});
+    // this.dbRef.on('value', data => console.dir(data));
   };
 
-  // setNum = (test) => this.favoriteNumber = (test) ? this.favoriteNumber++ : this.favoriteNumber--;
   setNum = (test): void => {
-    console.log('running setNum, test: ' + test);
-    if (test) {
-      this.favoriteNumber++;
-    } else {
-      this.favoriteNumber--;
-    }
-    // this.favoriteNumber = (test) ? ++this.favoriteNumber : --this.favoriteNumber;
+    this.favoriteNumber = (test) ? ++this.favoriteNumber : --this.favoriteNumber;
   }
+  updateNum = (): void => {
+    this.itemsRef.update({favoriteNumber: this.favoriteNumber});
+  };
 
   increaseNum = () => this.favoriteNumber++;
   decreaseNum = () => this.favoriteNumber--;
